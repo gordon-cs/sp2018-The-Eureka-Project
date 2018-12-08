b4ff9a33b0876270382321a9bc372d42e8e0454d
Dominique Adevai <dominiqueadevai@gmail.com>
2018-12-08 16:35:32 -0500

    Add Choice component for GamePlay

--- forwords/screens/GamePlay/components/Choice.js
+++ forwords/screens/GamePlay/components/Choice.js
@@ -2,30 +2,58 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     Text,
-    View,
     Platform,
-    Button,
+    TouchableOpacity,
 } from 'react-native';
 
 export default class Choice extends Component {
-constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state= {
-        title: this.props
-    };
-}
+        this.state = {
+            title: this.props,
+            promptID: this.props.promptID,
+            choiceID: this.props.choiceID,
+            correctAnswer: Boolean,
+        };
+        this.handleAttempt = this.handleAttempt.bind(this);
+    }
 
-
+    handleAttempt() {
+        if (this.state.promptID === this.state.choiceID) {
+            this.setState({ correctAnswer: true });
+        } else {
+            this.setState({ correctAnswer: false }); 
+        }
+    }
 
     render() {
+        // const backgroundColor = this.state.correctAnswer ? 'green' : 'red';
         return (
-            <View >
-                <Text>
+            <TouchableOpacity style={styles.mainContainer}
+                onPress={this.handleAttempt}>
+                <Text style={styles.choiceText}>
                     {this.props.text}
                 </Text>
-            </View>
+            </TouchableOpacity>
         )
     }
 }
 
+const styles = StyleSheet.create({
+    mainContainer: {
+        justifyContent: "center",
+        flex: 1,
+        margin: 10,
+        paddingTop: Platform.OS === "ios" ? 20 : 0,
+        width: 120,
+        height: 100,
+        borderRadius: 80,
+        backgroundColor: "white",
+    },
+    choiceText: {
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+});
