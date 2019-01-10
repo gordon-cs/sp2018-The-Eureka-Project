49f151a609cee99d31b24cd0c66e390bf9e977ec
Dominique Adevai <dominiqueadevai@gmail.com>
2019-01-10 00:45:39 -0500

    Refactor code a little bit

--- forwords/navigation/MainTabNavigator.js
+++ forwords/navigation/MainTabNavigator.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
-
-import TabBarIcon from '../components/TabBarIcon';
+import { createStackNavigator } from 'react-navigation';
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
-import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import GamePlayScreen from '../screens/GamePlay/GamePlayScreen';
 import SinglePlayerModeSelectionScreen from '../screens/SinglePlayerModeSelectionScreen';
@@ -13,87 +9,26 @@ import RegisterScreen from '../screens/RegisterScreen';
 import InstructionScreen from '../screens/InstructionScreen';
 import PlaceholderScreen from '../screens/PlaceholderScreen';
 
-
-
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
 });
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
-
 const LoginStack = createStackNavigator({
   Login: LoginScreen,
 });
-
-LoginStack.navigationOptions = {
-  tabBarLabel: 'Login',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-login'}
-    />
-  ),
-};
 
 const PlaceholderStack = createStackNavigator({
   Placeholder: PlaceholderScreen,
 });
 
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
-
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
-    />
-  ),
-};
-
 const RegisterStack = createStackNavigator({
   Register: RegisterScreen,
 });
 
-RegisterStack.navigationOptions = {
-  tabBarLabel: 'Register',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
-    />
-  ),
-}
-
 const SettingsStack = createStackNavigator({
   Settings: SettingsScreen,
 });
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
-    />
-  ),
-};
-
 
 const SinglePlayerModeSelectionStack = createStackNavigator({
   SinglePlayerModeSelection: SinglePlayerModeSelectionScreen,
@@ -109,10 +44,9 @@ const InstructionStack = createStackNavigator({
 
 export default createStackNavigator({
   LoginStack,
+  RegisterStack,
   HomeStack,
   PlaceholderStack,
-  LinksStack,
-  RegisterStack,
   SettingsStack,
   SinglePlayerModeSelectionStack,
   GamePlayScreenStack,
