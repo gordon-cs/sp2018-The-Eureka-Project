7a1de20aabf6bf7382fe29cd37001148d34a6299
Dominique Adevai <dominiqueadevai@gmail.com>
2019-01-10 00:42:35 -0500

    Login works with Firebase, routes to Home screen on successful login

--- forwords/screens/LoginScreen.js
+++ forwords/screens/LoginScreen.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import {
   Button,
-  Platform,
-  ScrollView,
   StyleSheet,
   Text,
   Alert,
@@ -28,16 +26,15 @@ export default class LoginScreen extends React.Component {
   }
 
   onLoginPress = () => {
-    console.log("GOT INTO onloginpress")
+    const { navigate } = this.props.navigation;
     firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
       .then(() => {
-        console.log("success onLoginPress")
+        navigate('Home');
       }, (error) => {
         Alert.alert(error.message);
-        console.log("failure onLoginPress")
-      }
-      );
+      });
   }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
@@ -49,7 +46,7 @@ export default class LoginScreen extends React.Component {
           <TextInput
             style={{ height: 60, width: 200 }}
             placeholder="Email"
-            onChangeText={(text) => this.setState({ text })}
+            onChangeText={(email) => this.setState({ email })}
             autoCorrect={false}
             autoCapitalize="none"
             returnKeyType="next"
@@ -57,7 +54,7 @@ export default class LoginScreen extends React.Component {
           <TextInput
             style={{ height: 60, width: 200 }}
             placeholder="Password"
-            onChangeText={(text) => this.setState({ text })}
+            onChangeText={(password) => this.setState({ password })}
             autoCorrect={false}
             autoCapitalize="none"
             secureTextEntry={true}
@@ -65,7 +62,7 @@ export default class LoginScreen extends React.Component {
           />
           <Button style={styles.button}
             title='Log In'
-            onPress={() => navigate('Home')}
+            onPress={() => this.onLoginPress()}
             color='purple'
           />
           <Button style={styles.button}
@@ -89,89 +86,14 @@ const styles = StyleSheet.create({
     flex: 10,
     backgroundColor: '#fff',
     padding: 10,
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
+  },  
   contentContainer: {
     paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
   },
   getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
     marginVertical: 150,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
   button: {
     alignItems: 'center',
