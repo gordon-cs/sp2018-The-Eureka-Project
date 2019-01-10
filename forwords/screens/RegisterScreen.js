1e54d10978112eaaf4df947e759cbe75f51de228
Dominique Adevai <dominiqueadevai@gmail.com>
2019-01-10 00:51:20 -0500

    Makes register screen functional

--- forwords/screens/RegisterScreen.js
+++ forwords/screens/RegisterScreen.js
@@ -1,146 +1,156 @@
-import axios from 'axios';
-import React, {Component} from 'React';
-import {Button,
+import axios from "axios";
+import React, { Component } from "React";
+import {
+    Button,
     StyleSheet,
     Text,
     TextInput,
-    View} from 'react-native';
+    View,
+    Alert,
+    KeyboardAvoidingView
+} from "react-native";
+import * as firebase from "firebase";
 
-var backwordsIP = '172.27.43.141';
+var backwordsIP = "172.27.43.141";
 
 export default class RegisterScreen extends Component {
     static navigationOptions = {
-        header: null,
+        header: null
     };
-
     constructor(props) {
         super(props);
-        
-        this.registerStudent = this.registerStudent.bind(this);
-        this.registerTeacher = this.registerTeacher.bind(this);
-
         this.state = {
-            firstName:  '',
-            lastName: '',
-            email: '',
-            password: '',
-            role: '',
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+            role: ""
         };
     }
 
-    registerStudent() {
-        let data = {
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            email: this.state.email,
-            password: this.state.password,
-            role: 'Student',         
+    registerStudent = () => {
+        if (this.state.password !== this.state.confirmPassword) {
+            Alert.alert("Passwords do not match");
+            return;
         }
-        // console.log('before registerStudent try statement', data)
-        // try {
-        //     console.log('entered try block')
-        //     axios.post('http://' + backwordsIP + ':8080' + '/user', data);
-        //     // console.log(res);
-        //     console.log("Student registered");
-        // } catch (err) {
-        //     throw new Error('Post /users did not work');
-        // }
-        const {navigate} = this.props.navigation;
-        navigate('Login');
-    }
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(
+                () => { },
+                error => {
+                    Alert.alert(error.message);
+                }
+            );
+    };
 
-    registerTeacher() {
-        let data = {
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            email: this.state.email,
-            password: this.state.password,
-            role: 'Teacher',
+    registerTeacher = () => {
+        if (this.state.password !== this.state.confirmPassword) {
+            Alert.alert("Passwords do not match");
+            return;
         }
-        const {navigate} = this.props.navigation;
-        navigate('Login');
-    }
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(
+                () => { },
+                error => {
+                    Alert.alert(error.message);
+                }
+            );
+    };
 
     render() {
-        const {navigate} = this.props.navigation;
-        let content;
+        const { navigate } = this.props.navigation;
         return (
-            <View style={styles.container}>
+            <KeyboardAvoidingView style={styles.container} behavior="position">
                 <View style={styles.contentContainer}>
-                <Text style={styles.title}>
-                    Register!
-                </Text>
-                <TextInput
-                    onChangeText={(firstName) => this.setState({firstName})}
-                    placeholder='First Name'
-                    returnKeyType= "next"
-                    style={{height: 60, width: 200}}
-                />
-                <TextInput 
-                    onChangeText={(lastName) => this.setState({lastName})}
-                    placeholder='Last Name' 
-                    returnKeyType= "next"
-                    style={{height: 60, width: 200}}
-                />
-                <TextInput 
-                    onChangeText={(email) => this.setState({email})}
-                    placeholder='Email'
-                    returnKeyType= "next"
-                    style={{height: 60, width: 200}}/>
-                <TextInput 
-                    onChangeText={(password) => this.setState({password})}
-                    placeholder='Password' 
-                    returnKeyType= "next"
-                    secureTextEntry={true}
-                    style={{height: 60, width: 200}}
-                />
-                <TextInput 
-                    placeholder='Confirm Password'
-                    secureTextEntry={true}
-                    style={{height: 60, width: 200}} />
-                <Text>
-                    Register as...
-                </Text>
-                <Button 
-                    onPress = {this.registerStudent}
-                    style={styles.button}
-                    title='Student'/>
-                <Button 
-                    onPress = {this.registerTeacher}
-                    style={styles.button}
-                    title='Teacher'/>
-                <Button
-                    onPress = {() => navigate('Login')}
-                    title = 'Cancel'/>
+                    <Text style={styles.title}>Register!</Text>
+                    <TextInput
+                        onChangeText={firstName => this.setState({ firstName })}
+                        placeholder="First Name"
+                        returnKeyType="next"
+                        style={{ height: 60, width: 200 }}
+                    />
+                    <TextInput
+                        onChangeText={lastName => this.setState({ lastName })}
+                        placeholder="Last Name"
+                        returnKeyType="next"
+                        style={{ height: 60, width: 200 }}
+                    />
+                    <TextInput
+                        onChangeText={email => this.setState({ email })}
+                        placeholder="Email"
+                        returnKeyType="next"
+                        style={{ height: 60, width: 200 }}
+                    />
+                    <TextInput
+                        onChangeText={password => this.setState({ password })}
+                        placeholder="Password"
+                        returnKeyType="next"
+                        secureTextEntry={true}
+                        style={{ height: 60, width: 200 }}
+                    />
+                    <TextInput
+                        onChangeText={confirmPassword => this.setState({ confirmPassword })}
+                        placeholder="Confirm Password"
+                        secureTextEntry={true}
+                        style={{ height: 60, width: 200 }}
+                    />
+                    <Text>Register as...</Text>
+                    <View style={styles.buttonsContainer}>
+                        <Button
+                            onPress={() => this.registerStudent()}
+                            style={styles.button}
+                            title="Student"
+                            color='purple'
+                        />
+                        <Button
+                            onPress={() => this.registerTeacher()}
+                            style={styles.button}
+                            title="Teacher"
+                            color='purple'
+                        />
+                    </View>
+                    <Button onPress={() => navigate("Login")}
+                        style={styles.button} title="Cancel"
+                        color='black' />
                 </View>
-            </View>
-        )
+            </KeyboardAvoidingView>
+        );
     }
 }
 
 const styles = StyleSheet.create({
     button: {
-        alignItems: 'center',
+        alignItems: "center",
         color: '#800080',
         borderRadius: 50,
         width: 160,
-        marginVertical: 30,
+        fontSize: 20,
+        fontWeight: "bold",
+        marginVertical: 30
     },
     container: {
         flex: 10,
-        backgroundColor: '#fff',
-        padding: 10,
+        backgroundColor: "#fff",
+        padding: 10
     },
     contentContainer: {
-        alignItems: 'center',
+        alignItems: "center",
         marginHorizontal: 50,
-        marginVertical: 150,
+        marginVertical: 50
     },
     title: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#800080',
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#800080",
         marginVertical: 10,
-        fontSize: 30,
+        fontSize: 30
+    },
+    buttonsContainer: {
+        justifyContent: "center",
+        flexDirection: "row"
     }
-})
+});
