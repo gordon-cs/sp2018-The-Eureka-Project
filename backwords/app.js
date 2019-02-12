2e42597f3e5baa36fbca9b6887541ed72def35d2
Dominique Adevai <dominiqueadevai@gmail.com>
2019-02-12 12:32:28 -0500

    Refactor lesson-list route to reflect MySQL DB changes

--- backwords/app.js
+++ backwords/app.js
@@ -68,7 +68,7 @@ app.get('/targetLanguage', function (req, res) {
 
 app.get('/lesson-list', function (req, res) {
 	console.log("in /lesson-list route in backend");
-	connection.query('SELECT * FROM Lessons;', function (error, results, fields) {
+	connection.query('SELECT * FROM lesson;', function (error, results, fields) {
 		if (error)
 			throw error;
 		res.json(results);
