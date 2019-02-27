4ca14e08169829ff020f1ff67dfca28f91aa42d6
Dominique Adevai <dominiqueadevai@gmail.com>
2019-02-27 15:29:48 -0500

    Add Google Translate api request

--- backwords/app.js
+++ backwords/app.js
@@ -8,8 +8,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
-// Google Translate API
-
+/* Google Translate API 
 // Imports the Google Cloud client library
 const {Translate} = require('@google-cloud/translate');
 
@@ -20,7 +19,6 @@ const projectId = 'ceramics-228616';
 const translate = new Translate({
   projectId: projectId,
 });
-
 
 // The text to translate
 const text = 'Hello, world!';
@@ -39,44 +37,45 @@ translate
   .catch(err => {
     console.error('ERROR:', err);
   });
+*/
 
+async function quickstart(
+  projectId = 'ceramics-228616' // Your GCP Project Id
+) {
 
+  console.log("in quickstart()!!!!!!!!");
+  // Imports the Google Cloud client library
+  const { Translate } = require('@google-cloud/translate');
 
-  async function quickstart(
-    projectId = 'ceramics-228616' // Your GCP Project Id
-  ) {
-    // Imports the Google Cloud client library
-    const {Translate} = require('@google-cloud/translate');
-  
-    // Instantiates a client
-    const translate = new Translate({projectId});
-  
-    // The text to translate
-    const text = 'Hello, world!';
-  
-    // The target language
-    const target = 'es';
-  
-    // Translates some text into Russian
-    const [translation] = await translate.translate(text, target);
-    console.log(`Text: ${text}`);
-    console.log(`Translation: ${translation}`);
-  }
+  // Instantiates a client
+  const translate = new Translate({ projectId });
+
+  // The text to translate
+  const text = 'Hello, world!';
+
+  // The target language
+  const target = 'es';
+
+  // Translates some text into Spanish
+  const [translation] = await translate.translate(text, target);
+  console.log(`Text: ${text}`);
+  console.log(`Translation: ${translation}`);
+}
 
 
 
 // Body Parser Middleware
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 //CORS Middleware
 app.use(function (req, res, next) {
-    //Enabling CORS 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
-    next();
-   });
-   
+  //Enabling CORS 
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+  next();
+});
+
 //Setting up server
 var server = app.listen(process.env.PORT || 8080, function () {
   var port = server.address().port;
@@ -84,49 +83,56 @@ var server = app.listen(process.env.PORT || 8080, function () {
 });
 
 var connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : '',
-  database : 'forwords'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'forwords'
 });
 
-connection.connect(function(err) {
-	if (err) {
-		console.error('Error connecting: ' + err.stack);
-		return;
-	}
-	console.log ('Connected as id ' + connection.threadId);
+connection.connect(function (err) {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id ' + connection.threadId);
 });
 
-app.get('/', function(req, res) {
-	res.send("Welcome to forwords");
+app.get('/', function (req, res) {
+  res.send("Welcome to forwords");
 });
+
+app.get('/translate/:q', function (req, res) {
+  console.log("in /translate/:q route in backend");
+  console.log("received as q: ", q);
+  res.send(quickstart(q));
+});
+
 
 app.get('/people', function (req, res) {
-	console.log("in /people route in backend");
-	connection.query('SELECT * FROM Users', function (error, results, fields) {
-		if (error)
-			throw error;
-		res.json(results);
-	});
+  console.log("in /people route in backend");
+  connection.query('SELECT * FROM Users', function (error, results, fields) {
+    if (error)
+      throw error;
+    res.json(results);
+  });
 })
 
 app.get('/targetLanguage', function (req, res) {
-	console.log("in /targetLanguage route in backend");
-	connection.query('SELECT TargetLanguage FROM Users WHERE FirstName = "Nikki"', function (error, results, fields) {
-		if (error)
-			throw error;
-		res.json(results);
-	});
+  console.log("in /targetLanguage route in backend");
+  connection.query('SELECT TargetLanguage FROM Users WHERE FirstName = "Nikki"', function (error, results, fields) {
+    if (error)
+      throw error;
+    res.json(results);
+  });
 })
 
 app.get('/lesson-list', function (req, res) {
-	console.log("in /lesson-list route in backend");
-	connection.query('SELECT * FROM lesson;', function (error, results, fields) {
-		if (error)
-			throw error;
-		res.json(results);
-	});
+  console.log("in /lesson-list route in backend");
+  connection.query('SELECT * FROM lesson;', function (error, results, fields) {
+    if (error)
+      throw error;
+    res.json(results);
+  });
 })
 
 // Gets a single word, for multiplayer
@@ -148,18 +154,18 @@ app.get('/choices/:lesson/:first/:second/:third/:fourth', function (req, res) {
   var second = req.params.second;
   var third = req.params.third;
   var fourth = req.params.fourth;
-  connection.query('SELECT * FROM Lesson' + lesson + ' WHERE ID in (' + first +',' + second + ',' + third + ',' + fourth + ');', function (error, results, fields) {
+  connection.query('SELECT * FROM Lesson' + lesson + ' WHERE ID in (' + first + ',' + second + ',' + third + ',' + fourth + ');', function (error, results, fields) {
     if (error)
       throw error;
     res.json(results);
   });
 })
 
-app.get('/lesson-words/:lesson', function(req, res) {
+app.get('/lesson-words/:lesson', function (req, res) {
   console.log('in /lesson-words from backend');
   var lesson = req.params.lesson;
   console.log('url param lesson: ', lesson);
-  connection.query('SELECT * FROM Lesson' + lesson +';', function (error, results, fields) {
+  connection.query('SELECT * FROM Lesson' + lesson + ';', function (error, results, fields) {
     if (error)
       throw error;
     res.json(results);
@@ -168,28 +174,28 @@ app.get('/lesson-words/:lesson', function(req, res) {
 
 //POST API
 // app.post('/user', data);
-app.post('/user', function(req , res){
+app.post('/user', function (req, res) {
   // res.sendStatus(200);
   // res.send('Post request sent');
   // console.log(res.data);
-  var query = "INSERT INTO Users (FirstName,LastName, TargetLanguage) VALUES ('Russ','Tuck','CN'); SELECT * FROM Users;" 
-  connection.query (query, function (error, results) {
+  var query = "INSERT INTO Users (FirstName,LastName, TargetLanguage) VALUES ('Russ','Tuck','CN'); SELECT * FROM Users;"
+  connection.query(query, function (error, results) {
     if (error)
-			throw error;
-		res.json(results);
+      throw error;
+    res.json(results);
   });
 });
 
 //PUT API
-app.put('/api/user/:id', function(req , res){
+app.put('/api/user/:id', function (req, res) {
   var query = "UPDATE Users SET LastName = Bjork WHERE FirstName = Russ;"
-//   executeQuery (res, query);
+  //   executeQuery (res, query);
 });
 
 // DELETE API
-app.delete('/user /:id', function(req , res){
+app.delete('/user /:id', function (req, res) {
   var query = "DELETE FROM Users WHERE FirstName=Russ;"
-//   executeQuery (res, query);
+  //   executeQuery (res, query);
 });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -202,20 +208,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.sendStatus(err.status || 500);
-	res.sendStatus(err.status);
-//  res.render('error');
+  res.sendStatus(err.status);
+  //  res.render('error');
 });
 
 module.exports = app;
