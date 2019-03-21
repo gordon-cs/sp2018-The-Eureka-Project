5ec28d5fc10041cfe2a7dd6f517bcfc91382b4b5
Dominique Adevai <dominiqueadevai@gmail.com>
2019-03-21 11:17:07 -0400

    Change server port back to 8080

--- backwords/app.js
+++ backwords/app.js
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
 });
 
 //Setting up server
-var server = app.listen(process.env.PORT || 7777, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
