a042f212fea48d196867d31116454589cab0599e
Dominique Adevai <dominiqueadevai@gmail.com>
2019-04-28 14:57:22 -0400

    Add a preemptive screen to adding a course

---
+++ forwords/screens/Launch/AddCourse/RoleSelectionScreen.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  Alert,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
+import forwordsStyles from "../../../constants/forwordsStyles";
+
+export default class RoleSelection extends Component {
+  static navigationOptions = {
+    header: null
+  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      role: ""
+    };
+  }
+
+  teacherAddCourse() {
+    const { navigate } = this.props.navigation;
+    navigate("AddCourse", { role: "teacher" });
+  }
+
+  studentAddCourse() {
+    const { navigate } = this.props.navigation;
+    navigate("AddCourse", { role: "student" });
+  }
+
+  render() {
+    return (
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={forwordsStyles.container}
+      >
+        <View style={forwordsStyles.headingView}>
+          <Text style={forwordsStyles.headingText}>
+            Which best describes the kind of course you want to add?
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={forwordsStyles.longButton}
+          onPress={() => this.teacherAddCourse()}
+        >
+          <Text style={forwordsStyles.buttonText}>
+            I am a teacher creating a course.
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={forwordsStyles.longButton}
+          onPress={() => this.studentAddCourse()}
+        >
+          <Text style={forwordsStyles.buttonText}>
+            I am a student joining a course.
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
+    );
+  }
+}
