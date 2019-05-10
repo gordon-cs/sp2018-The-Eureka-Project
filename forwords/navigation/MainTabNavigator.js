ddfa4125f582d2b8dfde82dd19c36a00887efcef
Dominique Adevai <dominiqueadevai@gmail.com>
2019-05-10 12:52:27 -0400

    Add home icon to tab bar

--- forwords/navigation/MainTabNavigator.js
+++ forwords/navigation/MainTabNavigator.js
@@ -3,7 +3,6 @@ import { Platform } from "react-native";
 import {
   createStackNavigator,
   createBottomTabNavigator,
-  createSwitchNavigator
 } from "react-navigation";
 
 import TabBarIcon from "../components/TabBarIcon";
@@ -11,8 +10,6 @@ import TabBarIcon from "../components/TabBarIcon";
 // Main Tabs
 import HomeScreen from "../screens/Launch/HomeScreen";
 import UserProfileScreen from "../screens/Launch/UserProfileScreen";
-
-import LoginScreen from "../screens/Authentication/LoginScreen";
 
 // Rest of the Screens
 import GamePlayScreen from "../screens/GamePlay/GamePlayScreen";
@@ -40,7 +37,7 @@ UserProfileStack.navigationOptions = {
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === "ios" ? "ios-person" : "md-link"}
+      name={Platform.OS === "ios" ? "ios-person" : "md-person"}
     />
   )
 };
@@ -54,8 +51,7 @@ HomeStack.navigationOptions = {
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === "ios" ? "ios-options" : "md-link"}
-
+      name={Platform.OS === "ios" ? "ios-home" : "md-home"}
     />
   )
 };
@@ -63,31 +59,10 @@ HomeStack.navigationOptions = {
 // Create Tab Navigator
 const TabNavigator = createBottomTabNavigator(
   {    
-    Home: HomeScreen,
-    Profile: UserProfileStack,
+    HomeStack,
+    UserProfileStack,
   },
   {
-    defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, horizontal, tintColor }) => {
-        const { routeName } = navigation.state;
-        let IconComponent = Ionicons;
-        let iconName;
-        if (routeName === "Home") {
-          iconName = `ios-information-circle${focused ? "" : "-outline"}`;
-          // Sometimes we want to add badges to some icons.
-          // You can check the implementation below.
-          IconComponent = HomeIconWithBadge;
-        } else if (routeName === "Profile") {
-          iconName = `ios-home`;
-        }
-        else {
-
-        }
-
-        // You can return any component that you like here!
-        return <IconComponent name={iconName} size={25} color={"#5b3b89"} />;
-      }
-    }),
     tabBarOptions: {
       activeTintColor: "#5b3b89",
       inactiveTintColor: "gray"
